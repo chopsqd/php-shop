5cb361e484e616d092cbcd90298b976a04af0252
Michael Chop <C:\Users\Super\AppData\Roaming\The Bat! Pwd>
2022-01-27 23:26:40 +0300

    First commit

---
+++ core/base/controllers/RouteController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace core\base\controllers;
+
+class RouteController
+{
+    static private $_instance;
+
+    static public function getInstance() {
+        if(self::$_instance instanceof self) {
+            return self::$_instance;
+        }
+
+        return  self::$_instance = new self;
+    }
+}
